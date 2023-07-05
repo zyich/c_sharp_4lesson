7604165d211a89ec11786da02e030e64c477dea5
zyich <zuilinho@yandex.ru>
2023-07-05 18:09:42 +0200

    all tasks

---
+++ task1_matrix /Program.cs
@@ -0,0 +1,63 @@
+﻿/*string[,] table = new string[2,5];
+// String.Empty
+// тут будет отсчет идти от 0 до 4
+// table[0,0]     table[0,1]  table[0,2] table[0,4]
+// table[1,0]     table[1,1]  table[1,2] table[1,4] 
+table[1,2] = "слово";
+
+
+for (int rows = 0; rows < 2; rows++)
+    {
+        for (int columns = 0; columns < 5; columns++)
+        {
+            Console.WriteLine($"-{table[rows, columns]}-");
+        }
+    }
+*/
+ 
+ 
+ 
+ 
+ /*int[,] matrix = new int [3,4];   
+
+ for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+*/
+
+
+  void PrintArray(int[,] matr)
+  {
+
+  
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+  }
+
+void FillArray(int[,] matr)
+    {
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            for (int j = 0; j < matr.GetLength(1); j++)
+            {
+                matr[i,j] = new Random().Next(1,10);// [1;10)
+            }
+        }
+    }
+  int[,] matrix = new int [3,4];
+  PrintArray(matrix);
+  FillArray(matrix);
+  Console.WriteLine();
+  PrintArray(matrix);
---
+++ task2_reccurent/Program.cs
@@ -0,0 +1,8 @@
+﻿int Factorial (int n)
+{
+    //1! = 1
+    // 0! = 1
+    if (n ==1 ) return 1;
+    else return n * Factorial(n-1);
+}
+Console.WriteLine(Factorial(5));
